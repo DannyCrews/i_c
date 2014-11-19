d19da6048c19d61f16c81f024c33f9afbc4a75a9
Dan Crews <dancrews@me.com>
2014-11-19 16:32:40 -0500

    add comments

--- area.c
+++ area.c
@@ -1,10 +1,28 @@
+/* This program calculates the area
+ * of a circle who's radius is stored
+ * in the variable radius */
+
+ /* #define PI 3.141593 - could have been used instead of calling area.h header file */
+
 #include <stdio.h>
 #include "area.h"
 
+/* comments begin here
+multiple lines
+and end here */
+
+// is used for C99 comments
+
 int main(void)
 {
-  float area, radius=2.5;
+  float area;       /* area if the circle */
+  float radius=2.5; /* radius of the circle */
+
+  /* calculate the area of a circle */
   area = PI*radius*radius;
+
+  /* print the values to the standard output */
   printf("The area of a circle with radius %f is %f\n", radius, area);
+
   return 0;
 }
